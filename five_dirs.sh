addc421723f65d6231cce787a1eb2443b3872908
Zhixing Liu <zliu2356@wisc.edu>
2024-10-15 14:28:05 -0500

    add five_dirs

--- five_dirs.sh
+++ five_dirs.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+rm -rf five
+
+mkdir five
+
+for i in $seq(5); do
+  mkdir "five/dir$i"
+  for j in $seq(4); do
+    for ((k=1; k<=j; k++)); do
+      echo "$j" >> "five/dir$i/file$j"
+    done
+  done
+done
