377efadf64eb302ef03a997776b91df8b4269847
Zhixing Liu <zliu2356@wisc.edu>
2024-10-17 13:10:12 -0500

    add five_dirs.sh

--- five_dirs.sh
+++ five_dirs.sh
@@ -4,9 +4,9 @@ rm -rf five
 
 mkdir five
 
-for i in $seq(5); do
+for i in $(seq 5); do
   mkdir "five/dir$i"
-  for j in $seq(4); do
+  for j in $(seq 4); do
     for ((k=1; k<=j; k++)); do
       echo "$j" >> "five/dir$i/file$j"
     done
